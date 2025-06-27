a7e26c3038599ab4fc0e97154723135c8fad30ca
Greg Brail <gbrail@users.noreply.github.com>
2025-06-26 22:56:08 -0400

    Add a test that exercises specific fast property optimizations

---
+++ tests/testsrc/jstests/fast-property-white-box.js
@@ -0,0 +1,61 @@
+/*
+ * Test aspects of property operation that are affected by the
+ * fast property optimizations. This is designed to be run with
+ * RHINO_DEBUG_PROPERTIES=true to manually verify what's happening.
+ */
+load("testsrc/assert.js");
+
+function checkFooness(x) {
+  return x.foo === 'foo';
+}
+
+// Create an object with a shape and use it in a function that will
+// check its properties.
+let o1 = { foo: 'foo', bar: 'bar', baz: 'baz' }
+assertTrue(checkFooness(o1));
+// Make sure changing it works
+o1.foo = 'bar';
+assertFalse(checkFooness(o1));
+
+// Create an object of a different shape.
+let o2 = {};
+o2.baz = 'baz';
+o2.bar = 'bar';
+o2.foo = 'foo';
+assertTrue(checkFooness(o2));
+
+// And go back
+o1.foo = 'foo';
+assertTrue(checkFooness(o1));
+
+// Do it in a loop, should not see relinking
+for (var i = 0; i < 100; i++) {
+    let o3 = { foo: 'foo', bar: 'bar', baz: 'baz' }
+    assertTrue(checkFooness(o1));
+    assertTrue(checkFooness(o2));
+    assertTrue(checkFooness(o3));
+}
+
+// Set a function property, should also see fast linking there
+let of1 = {
+  foo: 'foo', bar: 'bar', baz: 'baz',
+  checkBarness: function() { return this.bar == 'bar'; }
+}
+// Should be a fast property get here too
+assertTrue(of1.checkBarness());
+
+// Set up an object prototype, make fast linking work here too
+function FastMaker() {
+  this.foo = 'foo';
+  this.bar = 'bar';
+  this.baz = 'baz';
+}
+
+FastMaker.prototype.checkBazness = function() { return this.baz == 'baz'; }
+
+let fm1 = new FastMaker();
+assertTrue(fm1.checkBazness());
+fm1.baz = 'bar';
+assertFalse(fm1.checkBazness());
+
+'success';
