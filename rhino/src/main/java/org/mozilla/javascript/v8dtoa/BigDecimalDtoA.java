3759556d43165a3b59805d73211106e6397b9394
Greg Brail <gbrail@users.noreply.github.com>
2025-09-26 01:56:50 -0700

    Fix trailing zeroes

--- rhino/src/main/java/org/mozilla/javascript/v8dtoa/BigDecimalDtoA.java
+++ rhino/src/main/java/org/mozilla/javascript/v8dtoa/BigDecimalDtoA.java
@@ -5,21 +5,30 @@ import java.math.BigDecimal;
 public class BigDecimalDtoA {
 
     public static String dtoa(BigDecimal d) {
+        // Save negativity for later to make formatting easier
         boolean negative = d.compareTo(BigDecimal.ZERO) < 0;
         if (negative) {
             d = d.negate();
         }
 
+        // Get the shortest representation of the unscaled value
+        int precision = d.precision();
+        int scale = d.scale();
         String unscaled = d.unscaledValue().toString();
-        if (d.scale() == 0) {
+
+        if (scale == 0) {
+            // No decimals at all
             return integerEquivalent(d, unscaled, negative);
-        } else if (d.scale() > 0) {
-            if (d.precision() <= d.scale()) {
-                return smallDecimal(d, unscaled, negative);
+        } else if (scale > 0) {
+            // Digits after the decimal point
+            if (precision <= scale) {
+                // And no digits before the decimal point
+                return smallDecimal(d, unscaled, negative, precision, scale);
             }
-            return normalDecimal(d, unscaled, negative);
+            return normalDecimal(d, unscaled, negative, precision, scale);
         } else {
-            return largeDecimal(d, unscaled, negative);
+            // No digits after the decimal point, and in fact trailing zeroes before
+            return largeDecimal(d, unscaled, negative, precision, scale);
         }
     }
 
@@ -32,36 +41,43 @@ public class BigDecimalDtoA {
         return buf.toString();
     }
 
-    private static String normalDecimal(BigDecimal d, String unscaled, boolean negative) {
+    private static String normalDecimal(
+            BigDecimal d, String unscaled, boolean negative, int precision, int scale) {
         var buf = new StringBuilder();
         if (negative) {
             buf.append('-');
         }
-        // TODO trim trailing zeroes
-        int wholePart = d.precision() - d.scale();
+        int wholePart = precision - scale;
         buf.append(unscaled.substring(0, wholePart));
-        buf.append('.');
-        buf.append(unscaled.substring(wholePart));
+        String decimalPart = stripTrailingZeroes(unscaled.substring(wholePart));
+        if (!decimalPart.isEmpty()) {
+            buf.append('.');
+            buf.append(decimalPart);
+        }
         return buf.toString();
     }
 
-    private static String smallDecimal(BigDecimal d, String unscaled, boolean negative) {
+    private static String smallDecimal(
+            BigDecimal d, String unscaled, boolean negative, int precision, int scale) {
         var buf = new StringBuilder();
         if (negative) {
             buf.append('-');
         }
-        int zeroes = d.scale() - d.precision();
+        int zeroes = scale - precision;
         if (zeroes > 5) {
             // Go exponential
             buf.append(unscaled.substring(0, 1));
-            buf.append('.');
-            buf.append(unscaled.substring(1));
+            String decimalPart = stripTrailingZeroes(unscaled.substring(1));
+            if (!decimalPart.isEmpty()) {
+                buf.append('.');
+                buf.append(decimalPart);
+            }
             buf.append("e-");
             buf.append(zeroes + 1);
         } else {
             // Just decimal
             buf.append("0.");
-            for (int i = 0; i < d.scale() - d.precision(); i++) {
+            for (int i = 0; i < scale - precision; i++) {
                 buf.append('0');
             }
             buf.append(unscaled);
@@ -69,19 +85,23 @@ public class BigDecimalDtoA {
         return buf.toString();
     }
 
-    private static String largeDecimal(BigDecimal d, String unscaled, boolean negative) {
+    private static String largeDecimal(
+            BigDecimal d, String unscaled, boolean negative, int precision, int scale) {
         var buf = new StringBuilder();
         if (negative) {
             buf.append('-');
         }
-        int zeroes = -d.scale();
+        int zeroes = -scale;
         if (zeroes > 5) {
             // Go exponential
             buf.append(unscaled.substring(0, 1));
-            buf.append('.');
-            buf.append(unscaled.substring(1));
+            String decimalPart = stripTrailingZeroes(unscaled.substring(1));
+            if (!decimalPart.isEmpty()) {
+                buf.append('.');
+                buf.append(decimalPart);
+            }
             buf.append("e+");
-            buf.append(zeroes + d.precision() - 1);
+            buf.append(zeroes + precision - 1);
         } else {
             // Just decimal, with extra zeroes
             buf.append(unscaled);
@@ -90,5 +110,16 @@ public class BigDecimalDtoA {
             }
         }
         return buf.toString();
+    }
+
+    private static String stripTrailingZeroes(String s) {
+        int lastZero = s.length();
+        while (lastZero >= 1 && s.charAt(lastZero - 1) == '0') {
+            lastZero--;
+        }
+        if (lastZero < s.length()) {
+            return s.substring(0, lastZero);
+        }
+        return s;
     }
 }
